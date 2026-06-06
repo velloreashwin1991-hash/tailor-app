c5e8a5e1e62222f8e0ee70b9376c4598db6fd58f
velloreashwin1991-hash <velloreashwin1991@gmail.com>
2026-06-06 22:01:56 +0530

    Update Customer.java

--- src/main/java/com/tailor/model/Customer.java
+++ src/main/java/com/tailor/model/Customer.java
@@ -12,18 +12,12 @@ public class Customer {
     private String name;
     private String phone;
 
-    public Customer() {}
-
-    public Customer(String name, String phone) {
-        this.name = name;
-        this.phone = phone;
-    }
-
     public Long getId() { return id; }
-    public String getName() { return name; }
-    public String getPhone() { return phone; }
-
     public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+
+    public String getPhone() { return phone; }
     public void setPhone(String phone) { this.phone = phone; }
 }
