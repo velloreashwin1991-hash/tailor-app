341e2e9cbe9a0374cd77d0315e6b0f4daeb2af8c
velloreashwin1991-hash <velloreashwin1991@gmail.com>
2026-06-06 22:03:41 +0530

    Update CustomerService.java

--- src/main/java/com/tailor/service/CustomerService.java
+++ src/main/java/com/tailor/service/CustomerService.java
@@ -1,25 +1,9 @@
 package com.tailor.service;
 
 import com.tailor.model.Customer;
-import com.tailor.repository.CustomerRepository;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-public class CustomerService {
-
-    private final CustomerRepository repo;
-
-    public CustomerService(CustomerRepository repo) {
-        this.repo = repo;
-    }
-
-    public Customer add(Customer c) {
-        return repo.save(c);
-    }
-
-    public List<Customer> getAll() {
-        return repo.findAll();
-    }
+public interface CustomerService {
+    Customer save(Customer customer);
+    List<Customer> getAll();
 }
